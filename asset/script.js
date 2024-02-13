ae4c0307a4dcd9b781f2e312d3bb35858c0d8d80
Tai1minh <128927903+Tai1minh@users.noreply.github.com>
2024-02-13 18:30:11 +0700

    Update script.js

--- asset/script.js
+++ asset/script.js
@@ -52,7 +52,7 @@ $('.kehalaman2').click(function() {
 });
 
 var i = 0;
-var txt = "Ribuan malam ku termenung sendiri, Menunggu seseorang untuk memelukku, Lewati ratusan mimpi tersimpan dalam sanubari, Sendiri di tengah kegelapan yang semu. Terimakasih engkau telah hadir untukku,\nmenemani kesepianku dan telah menjadi penerang dalam gelapku,  I Love You";
+var txt = "Chào em nhó,";
 var speed = 200;
 
 function JalaninTeks() {
